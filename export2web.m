1f97eaec357bd84491865f0dec28676ce65a85e9
Joseph Lim <lim@csail.mit.edu>
2013-09-10 13:34:54 -0400

    minor comments

--- export2web.m
+++ export2web.m
@@ -1,4 +1,8 @@
 function export2web(web_dir, filename, ims)
+% Helper function to write a cell array of images to the target directory
+%
+
+
     for i = 1:length(ims)
         if size(ims{i},3)==3
             imwrite(ims{i}, sprintf('%s%d_%d.png', web_dir, filename, i));
--- parse_opts.m
+++ parse_opts.m
@@ -1,4 +1,12 @@
 function opts = parse_opts(opts, default_opts)
+% Given all default opts, check if opts has any non supported option and also set all option to
+% default values if not set
+%
+% USAGE
+%  default_opts = {'timelimit', 5, 'shouldrun', true};
+%  opts.timelimit = 3;
+%  opts = parse_opts(opts, default_opts);
+
     for i = 1:length(default_opts)/2
         if ~isfield(opts, default_opts{i*2-1})
             opts = setfield(opts, default_opts{i*2-1}, default_opts{i*2});
